e4fac0cecb7b9a6fc1adefe0aab4e34e64935ca5
quansys13 <quansys13@gmail.com>
2020-01-03 11:30:30 +0900

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CategoryComponent } from './category/category.component';
+import { StoryComponent } from './story/story.component';
+import { StoreComponent } from './store/store.component';
+import { ServiceComponent } from './service/service.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'category', pathMatch:'full'},
+  {path:'category', component:CategoryComponent},
+  {path:'story', component:StoryComponent},
+  {path:'store', component:StoreComponent},
+  {path:'service', component:ServiceComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+import { StoryComponent } from './story/story.component';
+import { StoreComponent } from './store/store.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      FooterComponent,
+      CategoryComponent,
+      StoryComponent,
+      StoreComponent,
+      ServiceComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
